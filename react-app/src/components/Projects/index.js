f1497842eecf00d71bdf705652658011ae2ce312
tayynicc <digitalidentity21@gmail.com>
2021-09-26 16:08:48 -0700

    formatting started on the project page, in progress

--- react-app/src/components/Projects/index.js
+++ react-app/src/components/Projects/index.js
@@ -7,6 +7,7 @@ import { useParams } from 'react-router'
 import { getProjects } from '../../store/project'
 
 import Header from '../Header'
+import Footer from '../Footer'
 
 
 function Project(){
@@ -30,21 +31,45 @@ function Project(){
 
     }
 
+    const save = (id) => {
+        const button = document.getElementById('save-button') 
+        if (button.innerHTML === 'Project Saved!'){
+            button.innerHTML = 'Save This Project'
+            button.classList.remove('saved')
+        } else {
+           button.innerHTML = 'Project Saved!' 
+           button.classList.add('saved')
+        }
+        
+        // console.log(`saving`, button.innerHTML)
+    }
     
 
     return (
-        <>
+        <body className='project-body'>
             <Header /> 
             {project.map((pro) => (
                 <>
-                    <h1>{pro.title}</h1>
+                    <div className='project__title-container'>
+                        <h1>{pro.title}</h1>
+                    </div>
+                    
                     <div className='project__image-container'><img className ='product__image' src={pro.image_url}></img></div>
 
                     <div className='project__instructions-container'><p>{pro.instruction}</p></div>
 
                     <div className='project__durationCost-container'>
-                    <h2>Estimated Time to Complete</h2>
-                        {pro.duration}
+                        <div className='duration'>
+                           <h2>Estimated Time to Complete: </h2>
+                            {pro.duration} Minutes
+                        </div>
+                        
+                        <div className='save-project-tile'>
+                            <button id='save-button'  onClick={() => save(pro.id)} className='save__button'>Save This Project! <img src="https://img.icons8.com/ios-glyphs/30/000000/like--v2.png"/></button>
+                            
+                            
+                        </div>
+
                     </div>
 
                     <div className='project__supplies-container'>
@@ -58,11 +83,16 @@ function Project(){
                         <h3>$ {pro.cost}</h3>
                     </div>
 
-                    <div className='project__externalLinks-container'></div>
+                    <div className='project__externalLinks-container'>
+                        <h2>External Resources: </h2>
+                        <a href={pro.live_links}>{pro.live_links}</a>
+                    </div>
                 </>
             ))}
+
+            <Footer />
             
-        </>
+        </body>
     )
 }
 
