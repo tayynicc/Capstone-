ae19484223b495c70a8a3442273aeef0f8873e0d
Taylor <digitalidentity21@gmail.com>
2021-09-27 14:06:07 -0700

    update comment input div bug

--- react-app/src/components/Header/index.js
+++ react-app/src/components/Header/index.js
@@ -11,12 +11,12 @@ function Header(){
     return (
         <>
             <div className='header__container'>
+                <div className='logo__container'></div>
                 <a href='/create-project'> Create a New Project</a>
                 <a href='/home'>Home</a>
                 <a href='/account'>Account</a>
                 <LogoutButton />
             </div>
-
         </>
     )
 }
