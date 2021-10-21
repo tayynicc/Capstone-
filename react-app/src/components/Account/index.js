062ba4dc2aad88ae59086f98f14aaffd8c610e2c
Taylor <digitalidentity21@gmail.com>
2021-10-21 14:52:05 -0700

    tables mostly flushed out

--- react-app/src/components/Account/index.js
+++ react-app/src/components/Account/index.js
@@ -44,7 +44,6 @@ function Account(){
     const getProject = (id) => {
         const project = projects.filter((project) => project.id === id)
 
-        console.log(`!!`,project)
 
         const [ proj ] = project
         return proj
@@ -74,7 +73,6 @@ function Account(){
                 <h1 className='logo'>ReNew Me</h1>
            </div>
 
-            {/* <div className='UI__container-outter'> */}
                 <div className='UI__default-pfp'></div>
                 <div className='user__information-container'>
                     <div className='user__information-content'>
@@ -91,107 +89,118 @@ function Account(){
                             <h3>{user.username}</h3> 
                             </div>
 
-                            
-
-
-
-
 
                         </div>
 
-
-
-
-                        
-                        
                        
                     </div>
                 </div>
 
             
-            {/* </div> */}
-
-            <div className='container__labels'>
-                <thead className='tableTitle'>
-                    <tr>
-                        <th>My Projects</th>
-                    </tr>
-                </thead>
-                <thead className='table_columnLabel'>
-                    <tr>
-                        <th>Active Listing</th>
-                        <th>Title</th>
-                        <th>Type</th>
-                        <th>Created At</th>
-                        
-                    </tr>
-                </thead>
-                <tbody>
-                    {usersProjects?.map((project) => (
-                        <>
-                        <div className='data_row'>
-                            <tr >
-                                <td className='table__data'>Listing</td>
-                                <td className='table__data'>{project.title}</td>
-                                <tr className='table__data'>
-                                    <td className='table__data'>{project.action}</td>
-                                    <td className='table__data'>{project.type}</td>
-                                </tr>
-                                
-                                <td className='table__data'>{project.created_at}</td>
-                                <td className='table__data'>Edit Delete</td>
+            <div className='table__containers'>
+                <div className='container__labels'>
+                    <div className='usersPd__table-label'>
+                        <thead className='tableTitle'>
+                            <tr>
+                                <th>My Projects</th>
                             </tr>
-                        </div>    
-                         
-                        </>
-                    ))}
+                        </thead>
+                    </div>
                     
-                </tbody>
-            </div>
-            
-            {/* <div className='container__labels'>
-                <h2 className='rotated__label'>My Projects</h2>
-                <h2 className='rotated__label'>My Saved Projects</h2>
-            </div>
+                    <div className='labels__containers'>
+                        <thead className='table_columnLabel'>
+                            
+                            <tr>
+                                <div className='center__labels'>
+                                    <div className='label-1'>
+                                        <th>Active Listing</th>
+                                    </div>
 
-            <div className='users__projects-container-account'>
-            
-            <h1>My Projects</h1>
-            {usersProjects?.map((project) => (
-                        <div className='tile__containers-account'>
-                            <div>
-                                <img alt='project image' className='tile-image-account' src={project.image_url}></img>
+                                    <div className='label-2'>
+                                        <th>Title</th>
+                                    </div>
+
+                                    <div className='label-3'>
+                                        <th>Type</th>
+                                    </div>
+
+                                    <div className='label-4'>
+                                        <th>Created At</th>
+                                    </div>
+                                </div>
+                            </tr>
+                            
+                        </thead>
+                    </div>
+                    <tbody>
+                        {usersProjects?.map((project) => (
+                            <>
+                            <div className='data_row'>
+                                <tr >
+                                    <div className='table__data-container'>
+                                        <div className='link'>
+                                            <td className='table__data link-data'><a className='a__link' href={`/projects/${project.id}`}><li>Active</li></a></td>
+                                        </div>
+                                        <div className='title'>
+                                            <td className='table__data title-data'>{project.title}</td>
+                                        </div>
+
+                                        <div className='tags'>
+                                            <td className='table__data tags-data'>{project.action} {project.type}</td>
+                                        </div>
+                                        
+                                        
+
+
+                                        <td className='table__data'>{project.created_at}</td>
+
+                                        <div className='editControls'>
+                                            <td className='table__data editCont'>Edit Delete</td>
+                                        </div>
+                                    </div>
+                                </tr>
+                            </div>    
+                            
+                            </>
+                        )).reverse()}
+                        
+                    </tbody>
+                </div>
+
+                <div className='savedProjects'>
+                    <div className='saved__table-label'>
+                        <thead>
+                            
+                                <tr className='saved__table'>
+                                    <th> My Saved Projects</th>
+                                </tr>
+                            
+                        </thead>
+                    </div>
+                    <tbody>
+                        {usersProjects?.map((project) => (
+
+                        
+                            <div className='data__container-saved'>
+                                <div className="image__container">
+                                    <td>
+                                        <img alt='project image' className='tile-image-account savedImg' src={project.image_url}></img>
+                                    </td>
+                                </div>
+
+                                <div className='saved__project-container'>
+                                    <a href={`/projects/${project.id}`}><td>{project.title}</td></a>
+                                </div>
+
+                                <div className='remove_saved'>
+                                    <button className='project__delete-btn' onClick={() => handleDelete(project.id)}>Remove</button>
+                                </div>
                             </div>
-                            <div className='tile-title-account'>
-                                <p>{project.title}</p>
-                            <div className='project__buttons'>
-                                <a href={`/update/${project.id}`}><img src="https://img.icons8.com/ios-glyphs/30/000000/edit--v1.png"/></a>
-                                <button className='project__delete-btn' onClick={() => handleDelete(project.id)}><img className='deleteImage' src="https://img.icons8.com/fluency/48/000000/delete-sign.png"/></button>
-                            </div>
-                                
-                            </div>
-                            // <button type='submit' onClick={() => click()}>Click me </button>
-                        </div>
+                        )).reverse()}
 
-                    )).reverse()}
-
-            </div> */}
-
-            {/* <div className='users__savedProjects-container'>
-                {savedProjects.map((project) => (
-                   
-                    <a className='anchor' href={`/projects/${project.project_id}`}>
-                    <div className='tile__containers-account'>
-                        <div>
-                            <img className='tile-image-account' src={getProject(project.project_id)?.image_url}></img>
-                        </div>
-                        <div className='tile-title-account'>
-                            <p>{getProject(project.project_id)?.title}</p>
-                        </div>
-                    </div></a>
-                )).reverse()}
-
-            </div> */}
+                    </tbody>
+                </div>
+           </div>
         </div>
         
     )
